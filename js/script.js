833a8e28d32c0748582937c2582107cb3436d151
Juliette <jcougnoux@gmail.com>
2016-02-23 02:56:33 +0100

    ajout des autres pages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(".header").on('click', function() {
     $(".menu").toggleClass("active");
-    $("p, .image, .integration, .js, .cms").toggleClass("hiden");
+    $("p, .image, .integration, .js, .cms, .je").toggleClass("hiden");
     $(".container-fluid").toggleClass("left");
     $(".hamb").toggleClass("cross");
   });
