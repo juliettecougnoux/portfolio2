11b2baeab04cbc71474990b4358cf5253eb67dd6
Juliette <jcougnoux@gmail.com>
2016-02-23 04:27:36 +0100

    modif styl ajout réseaux sociaux

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $(".header").on('click', function() {
     $(".menu").toggleClass("active");
-    $("p, .image, .integration, .js, .cms, .je").toggleClass("hiden");
+    $("p, .image, .integration, .js, .cms, .je, .contact, .rs").toggleClass("hiden");
     $(".container-fluid").toggleClass("left");
     $(".hamb").toggleClass("cross");
+    $("form").toggleClass("hiden");
   });
 })
